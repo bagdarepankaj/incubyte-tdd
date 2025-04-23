895620854718ffe950d6c56e300ffbcefee5bd2d
Pankaj Bagdare <105585524+bagdarepankaj@users.noreply.github.com>
2025-04-23 09:31:08 +0000

    Add second example and update method accordingly

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
 
     def add(str = "")
         return 0 if str.empty?
+
+        str.to_i
     end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,9 +3,19 @@ require "string_calculator"
 describe StringCalculator do
     describe '#add' do
         sc = StringCalculator.new
+
         context "when string is empty" do
             it 'return zero' do
                 expect(sc.add).to eq(0)
+            end
+        end
+
+        context "when string is not empty" do
+            context "having 1 number" do
+                result = sc.add("3")
+                it 'returns the number' do
+                    expect(result).to eq(3)
+                end
             end
         end
     end

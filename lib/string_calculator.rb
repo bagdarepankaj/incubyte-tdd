9c3c6e733e87bcc38d282ece93d2ae397511cfca
Pankaj Bagdare <105585524+bagdarepankaj@users.noreply.github.com>
2025-04-23 09:16:41 +0000

    Adding first rspec example and its 'add' method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,6 @@
 class StringCalculator
+
+    def add(str = "")
+        return 0 if str.empty?
+    end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,12 @@
 require "string_calculator"
 
 describe StringCalculator do
+    describe '#add' do
+        sc = StringCalculator.new
+        context "when string is empty" do
+            it 'return zero' do
+                expect(sc.add).to eq(0)
+            end
+        end
+    end
 end
